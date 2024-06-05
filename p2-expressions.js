a69517cf2a8b46e0e026991ae0c77bcb0e25d269
JaneDuncann <166860762+JaneDuncann@users.noreply.github.com>
2024-06-05 16:33:31 -0700

    Add files via upload

---
+++ p2-expressions.js
@@ -0,0 +1,50 @@
+/*
+    CIT 281 Project 2
+    Name: Jane Duncan
+*/
+
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  
+  let result = "";
+  
+  for (let i = 0; i < getRandomInteger(5, 26); i++) {
+    result += alphabet[getRandomInteger(1, alphabet.length - 1)];
+  }
+  
+  console.log(result);
+  
+  // return a single, random, lowercased letter
+  function getRandomLetter() {
+    let randomIndex = getRandomInteger(0, alphabet.length - 1);
+    result = alphabet[randomIndex];
+    return result.toLowerCase();
+  }
+  
+  for (let i = 0; i < 1; i++) {
+    console.log(getRandomLetter());
+  }//Is this correct? Is the for loop necessary 
+  
+  //retrun a random length string
+  function getRandomString(minLength, maxLength) {
+    result = "";
+    randomLength = getRandomInteger(minLength, maxLength);
+    for (let i = 0; i < randomLength; i++) {
+      result = result + getRandomLetter();
+    }
+    return result;
+  }
+  console.log(getRandomString(10, 20));
+  
+  //return a string in acsending (a-z) order
+  function getSortedString(string) {
+      return string.split("").sort().join(""); //Split turns the 
+      //string into an array so that we can use the sort method which only
+    //works on arrays 
+    }
+    console.log(getSortedString("xpacd"))
+  
